f267b5a097277e2147b02fc4e3409fdbdd7eb0d9
alihadyalyami <a.h.a.f.1430@gmail.com>
2020-09-21 11:39:49 +0300

    added local storage to recognize the last choice

--- script.js
+++ script.js
@@ -39,9 +39,12 @@ function switchTheme(event) {
 
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 
@@ -50,3 +53,13 @@ function switchTheme(event) {
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check localStorage for switchTheme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
